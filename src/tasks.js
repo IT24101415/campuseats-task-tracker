e219ac361e092293805e70bf68e86246f765da4f
IT24101415 <it24101415@my.sliit.lk>
2026-09-22 22:06:16 +0530

    feat: add initial CampusEats task list

---
+++ src/tasks.js
@@ -0,0 +1,9 @@
+// CampusEat// CampusEats task list
+
+const tasks = [
+  "Design the menu screen",
+  "Build the orders API",
+  "Add user login",
+];
+
+console.log(`CampusEats has ${tasks.length} open tasks`);s task list
